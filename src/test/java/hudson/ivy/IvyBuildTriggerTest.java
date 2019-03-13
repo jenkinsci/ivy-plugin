5e3e0780c0d5904cb9bd611fa723ebb2d2cad9e8
René Scheibe <rene.scheibe@gmail.com>
2019-03-13 19:41:30 +0100

    Add tests for JCasC compatibility (export to YAML)

--- src/test/java/hudson/ivy/IvyBuildTriggerTest.java
+++ src/test/java/hudson/ivy/IvyBuildTriggerTest.java
@@ -1,9 +1,14 @@
 package hudson.ivy;
 
+import hudson.ivy.IvyBuildTrigger.IvyConfiguration;
 import io.jenkins.plugins.casc.ConfigurationAsCode;
+import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -26,5 +31,26 @@ public class IvyBuildTriggerTest {
         assertThat(globalConfig.getConfigurations()[1].getIvyConfPath(), equalTo("dir/file2.xml"));
         assertThat(globalConfig.getConfigurations(), arrayWithSize(2));
         assertThat(globalConfig.isExtendedVersionMatching(), equalTo(true));
+    }
+
+    @Test
+    public void should_support_jcasc_to_yaml() throws Exception {
+        IvyBuildTrigger.DescriptorImpl globalConfig = j.jenkins.getDescriptorByType(IvyBuildTrigger.DescriptorImpl.class);
+
+        IvyConfiguration config1 = new IvyConfiguration("setting1", "dir/file1.xml");
+        IvyConfiguration config2 = new IvyConfiguration("setting2", "dir/file2.xml");
+        globalConfig.setConfigurations(new IvyConfiguration[]{ config1, config2 });
+        globalConfig.setExtendedVersionMatching(true);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ConfigurationAsCode.get().export(outputStream);
+        String exportedYaml = outputStream.toString("UTF-8");
+
+        InputStream yamlStream = getClass().getResourceAsStream(getClass().getSimpleName() + "/configuration-as-code.yml");
+        String expectedYaml = IOUtils.toString(yamlStream, "UTF-8")
+                .replaceAll("\r\n?", "\n")
+                .replace("unclassified:\n", "");
+
+        assertThat(exportedYaml, containsString(expectedYaml));
     }
 }
--- src/test/java/hudson/ivy/IvyModuleSetTest.java
+++ src/test/java/hudson/ivy/IvyModuleSetTest.java
@@ -1,9 +1,13 @@
 package hudson.ivy;
 
 import io.jenkins.plugins.casc.ConfigurationAsCode;
+import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -21,5 +25,23 @@ public class IvyModuleSetTest {
         ConfigurationAsCode.get().configure(yamlUrl);
 
         assertThat(globalConfig.getGlobalAntOpts(), equalTo("-Dproperty1=value1\n-Dproperty2=value2"));
+    }
+
+    @Test
+    public void should_support_jcasc_to_yaml() throws Exception {
+        IvyModuleSet.DescriptorImpl globalConfig = j.jenkins.getDescriptorByType(IvyModuleSet.DescriptorImpl.class);
+
+        globalConfig.setGlobalAntOpts("-Dproperty1=value1\n-Dproperty2=value2");
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ConfigurationAsCode.get().export(outputStream);
+        String exportedYaml = outputStream.toString("UTF-8");
+
+        InputStream yamlStream = getClass().getResourceAsStream(getClass().getSimpleName() + "/configuration-as-code.yml");
+        String expectedYaml = IOUtils.toString(yamlStream, "UTF-8")
+                .replaceAll("\r\n?", "\n")
+                .replace("unclassified:\n", "");
+
+        assertThat(exportedYaml, containsString(expectedYaml));
     }
 }
