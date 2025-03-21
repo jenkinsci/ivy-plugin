9df7c07e0857e8b2d9386554af5e1d654fbce241
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-21 09:32:31 +0100

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods

--- src/test/java/hudson/ivy/IvyModuleSetTest.java
+++ src/test/java/hudson/ivy/IvyModuleSetTest.java
@@ -5,37 +5,29 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 import io.jenkins.plugins.casc.ConfigurationAsCode;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.IOUtils;
-import org.junit.Rule;
-import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
+import org.junit.jupiter.api.Test;
 
-public class IvyModuleSetTest {
-
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+@WithJenkinsConfiguredWithCode
+class IvyModuleSetTest {
 
     @Test
-    public void should_support_jcasc_from_yaml() throws Exception {
+    @ConfiguredWithCode("IvyModuleSetTest/configuration-as-code.yml")
+    void should_support_jcasc_from_yaml(JenkinsConfiguredWithCodeRule j) {
         IvyModuleSet.DescriptorImpl globalConfig = j.jenkins.getDescriptorByType(IvyModuleSet.DescriptorImpl.class);
-
-        String yamlUrl = getClass()
-                .getResource(getClass().getSimpleName() + "/configuration-as-code.yml")
-                .toString();
-        ConfigurationAsCode.get().configure(yamlUrl);
 
         assertThat(globalConfig.getGlobalAntOpts(), equalTo("-Dproperty1=value1\n-Dproperty2=value2"));
     }
 
     @Test
-    public void should_support_jcasc_to_yaml() throws Exception {
-        IvyModuleSet.DescriptorImpl globalConfig = j.jenkins.getDescriptorByType(IvyModuleSet.DescriptorImpl.class);
-
-        globalConfig.setGlobalAntOpts("-Dproperty1=value1\n-Dproperty2=value2");
-
+    @ConfiguredWithCode("IvyModuleSetTest/configuration-as-code.yml")
+    void should_support_jcasc_to_yaml(JenkinsConfiguredWithCodeRule j) throws Exception {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ConfigurationAsCode.get().export(outputStream);
         String exportedYaml = outputStream.toString(StandardCharsets.UTF_8);
